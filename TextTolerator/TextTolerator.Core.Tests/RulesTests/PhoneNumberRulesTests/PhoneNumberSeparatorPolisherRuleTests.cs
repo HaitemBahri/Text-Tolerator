e3746be8601ae97837810eaa7fb2c8b7687ef370
haitembahri <haitem_bahri@yahoo.com>
2023-11-03 04:18:42 +0000

    changing PhoneNumberSeparatorCleanerRule to a PolisherRule

--- TextTolerator/TextTolerator.Core.Tests/RulesTests/PhoneNumberRulesTests/PhoneNumberSeparatorPolisherRuleTests.cs
+++ TextTolerator/TextTolerator.Core.Tests/RulesTests/PhoneNumberRulesTests/PhoneNumberSeparatorPolisherRuleTests.cs
@@ -2,21 +2,20 @@
 
 namespace TextTolerator.Core.Tests.RulesTests.PhoneNumberRulesTests
 {
-    public class PhoneNumberSeparatorCleanerRuleTests
+    public class PhoneNumberSeparatorPolisherRuleTests
     {
-        private PhoneNumberSeparatorCleanerRule _sut = new();
+        private PhoneNumberSeparatorPolisherRule _sut = new();
 
         public static TheoryData<string, List<string>, string> ShouldReturnCorrectResultTheoryData => new()
         {
             {"455 565000", new List<string>(){ "455 565000", "455565000" }, "Space"},
-            {"455 565 1000", new List<string>(){ "455 565 1000", "455 5651000", "455565 1000", "4555651000" }, "Space - twice"},
+            {"455 565 1000", new List<string>(){ "455 565 1000", "4555651000" }, "Space - twice"},
 
             {"455-565000", new List<string>(){ "455-565000", "455565000" }, "Hyphen"},
-            {"455-565-1000", new List<string>(){ "455-565-1000", "455-5651000", "455565-1000", "4555651000" }, "Hyphen - twice"},
+            {"455-565-1000", new List<string>(){ "455-565-1000", "4555651000" }, "Hyphen - twice"},
 
-            {"455 565-1000", new List<string>(){ "455 565-1000", "455 5651000", "455565-1000", "4555651000" }, "Space & Hyphen"},
-            {"(455) 565-89-33", new List<string>(){ "(455) 565-89-33", "(455) 565-8933", "(455) 56589-33", "(455) 5658933",
-                                                    "(455)565-89-33", "(455)565-8933", "(455)56589-33", "(455)5658933" }, "Space & Hyphen twice"},
+            {"455 565-1000", new List<string>(){ "455 565-1000", "4555651000" }, "Space & Hyphen"},
+            {"(455) 565-89-33", new List<string>(){ "(455) 565-89-33", "(455)5658933" }, "Space & Hyphen twice"},
         };
 
         [Theory]
---
+++ TextTolerator/TextTolerator.Core/Rules/PhoneNumberRules/PhoneNumberSeparatorPolisherRule.cs
@@ -0,0 +1,22 @@
+﻿using TextTolerator.Core.Attributes;
+using TextTolerator.Core.Rules.RulesBases;
+
+namespace TextTolerator.Core.Rules.PhoneNumberRules
+{
+    [PhoneNumberRule]
+    public class PhoneNumberSeparatorPolisherRule : IRule
+    {
+        protected List<PolisherRuleValue> PolisherRuleValues => new()
+        {
+            new PolisherRuleValue(" ", StringPosition.Start | StringPosition.Mid | StringPosition.End ),
+            new PolisherRuleValue("-", StringPosition.Start | StringPosition.Mid | StringPosition.End ),
+        };
+
+        public List<string> ProcessText(string inputText)
+        {
+            var polisherRuleBase = new PolisherRuleBase();
+
+            return polisherRuleBase.ProcessText(inputText, PolisherRuleValues);
+        }
+    }
+}
