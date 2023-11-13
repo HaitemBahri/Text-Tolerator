d81bf4ae049955cde7c2da9ccabc6a7885ffe4c7
haitembahri <haitem_bahri@yahoo.com>
2023-11-13 01:09:42 +0000

    adding FixedRulesProvider class

---
+++ TextTolerator/TextTolerator.Core.Tests/RulesProvidersTests/FixedRulesProviderTests.cs
@@ -0,0 +1,33 @@
+﻿using TextTolerator.Core.Rules;
+using TextTolerator.Core.Rules.ArabicRules;
+using TextTolerator.Core.Rules.EnglishRules;
+using TextTolerator.Core.RulesProviders;
+
+namespace TextTolerator.Core.Tests.RulesProvidersTests
+{
+    public class FixedRulesProviderTests
+    {
+        private FixedRulesProvider _sut;
+
+        public static TheoryData<List<IRule>, string> ShouldReturnCorrectRulesTheoryData => new()
+        {
+            {new List<IRule>() {
+                new ArabicAccentPolisherRule(),
+                new ArabicCommonLetterReplacerRule(),
+                new EnglishINGReplacerRule(),
+            }, "Mixed Rules" },
+
+        };
+
+        [Theory]
+        [MemberData(nameof(ShouldReturnCorrectRulesTheoryData))]
+        public void ShouldReturnCorrectResult(List<IRule> expectedResult, string testDataName)
+        {
+            _sut = new FixedRulesProvider(expectedResult.ToArray());
+
+            var genericRulesProviderTests = new GenericRulesProviderTests();
+
+            genericRulesProviderTests.ShouldReturnCorrectRules(_sut, expectedResult, testDataName);
+        }
+    }
+}
---
+++ TextTolerator/TextTolerator.Core/RulesProviders/FixedRulesProvider.cs
@@ -0,0 +1,18 @@
+﻿using TextTolerator.Core.Rules;
+
+namespace TextTolerator.Core.RulesProviders
+{
+    public class FixedRulesProvider : IRulesProvider
+    {
+        private readonly List<IRule> _rules = new();
+
+        public FixedRulesProvider(params IRule[] rules)
+        {
+            _rules = rules.ToList();
+        }
+        public IEnumerable<IRule> GetRules()
+        {
+            return _rules;
+        }
+    }
+}
