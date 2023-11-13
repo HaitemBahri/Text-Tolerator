a70bd5a445d3c342c25af70a4f83fd195fd4e54f
haitembahri <haitem_bahri@yahoo.com>
2023-11-13 02:48:58 +0000

    changing PolisherRulesProvider to PhoneNumberPolisher rules provider

--- TextTolerator/TextTolerator.Core.Tests/RulesProvidersTests/PhoneNumberPolisherRulesProviderTests.cs
+++ TextTolerator/TextTolerator.Core.Tests/RulesProvidersTests/PhoneNumberPolisherRulesProviderTests.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TextTolerator.Core.Attributes;
+﻿using TextTolerator.Core.Rules;
 using TextTolerator.Core.Rules.ArabicRules;
-using TextTolerator.Core.Rules.EnglishRules;
 using TextTolerator.Core.Rules.PhoneNumberRules;
-using TextTolerator.Core.Rules.RulesBases;
-using TextTolerator.Core.Rules.WebsiteRules;
-using TextTolerator.Core.Rules;
 using TextTolerator.Core.RulesProviders;
 
 namespace TextTolerator.Core.Tests.RulesProvidersTests
 {
-    public class PolisherRulesProviderTests
+    public class PhoneNumberPolisherRulesProviderTests
     {
-        private PolisherRulesProvider _sut = new();
+        private PhoneNumberPolisherRulesProvider _sut = new();
 
         public static TheoryData<List<IRule>, string> ShouldReturnCorrectRulesTheoryData => new()
         {
             {new List<IRule>() {
-                new ArabicAccentPolisherRule(),
                 new PhoneNumberBracketsPolisherRule(),
                 new PhoneNumberSeparatorPolisherRule(),
             }, "Polisher Rules" },
--- TextTolerator/TextTolerator.Core/RulesProviders/PhoneNumberPolisherRulesProvider.cs
+++ TextTolerator/TextTolerator.Core/RulesProviders/PhoneNumberPolisherRulesProvider.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TextTolerator.Core.Rules;
-using TextTolerator.Core.Rules.ArabicRules;
+﻿using TextTolerator.Core.Rules;
 using TextTolerator.Core.Rules.PhoneNumberRules;
 
 namespace TextTolerator.Core.RulesProviders
 {
-    public class PolisherRulesProvider : IRulesProvider
+    public class PhoneNumberPolisherRulesProvider : IRulesProvider
     {
         public IEnumerable<IRule> GetRules()
         {
             return new List<IRule>()
             {
-                new ArabicAccentPolisherRule(),
                 new PhoneNumberBracketsPolisherRule(),
                 new PhoneNumberSeparatorPolisherRule(),
             };
